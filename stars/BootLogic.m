2d71454a96589625718fa85125c5f4073c2cfeb8
bksasuke <student@mac-mini.local>
2015-10-10 16:43:40 +0700

    fields

---
+++ stars/BootLogic.m
@@ -0,0 +1,30 @@
+//
+//  BootLogic.m
+//  TechmasterApp
+//
+//  Created by techmaster on 9/7/14.
+//  Copyright (c) 2014 Techmaster. All rights reserved.
+//
+
+#import "BootLogic.h"
+#import "MainScreen.h"
+
+
+@implementation BootLogic
++ (void) boot: (UIWindow*) window
+{
+    MainScreen* mainScreen = [[MainScreen alloc] initWithStyle:UITableViewStyleGrouped];
+    //--------- From this line, please customize your menu data -----------
+    NSDictionary* basic = @{SECTION: @"Basic", MENU: @[
+                                    @{TITLE: @"StarsField", CLASS: @"StarsField"}
+                          ]};
+    
+    mainScreen.menu = @[basic];
+    mainScreen.title = @"Convert code: Obj-C -> Swift";
+    mainScreen.about = @"Design by Student";
+    //--------- End of customization -----------
+    UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController: mainScreen];
+    
+    window.rootViewController = nav;
+}
+@end
